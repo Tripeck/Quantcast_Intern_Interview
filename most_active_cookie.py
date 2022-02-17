dc7c894a70521b376d1a2df9e2996b8860f3500b
Tripeck <94490549+Tripeck@users.noreply.github.com>
2022-02-16 16:42:59 -0800

    Update most_active_cookie.py

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -13,24 +13,28 @@ import sys
     4sMM2LxV07bPJzwf
 """
 
+"""
+Primary Function.
+Returns the most active cookies. If there are multiple cookies that are tied, all of those cookies will be printed as well on new lines.
+"""
 def most_active_cookie(selected_date):
-    with open('cookies.txt') as f:
-        lines = f.readlines()
+    with open('cookies.txt') as f: # Reads the file.
+        lines = f.readlines() 
 
-    cookie_activity_map = {}
+    cookie_activity_map = {} # Map all of cookies accessed that day. The value is the number of occurences.
 
-    for line in lines:
-        cookie = line.split(',')
+    for line in lines: # Loops through each line in the document.
+        cookie = line.split(',') # Splits the key and date into to elements.
         key = cookie[0]
-        date = cookie[1][:10]
-        if date == selected_date:
-            if key not in cookie_activity_map:
+        date = cookie[1][:10] # date is the year, month, and day. We do not care about the time.
+        if date == selected_date: # the date of the cookie matches the date that we have selected.
+            if key not in cookie_activity_map: # The map is updated.
                 cookie_activity_map[key] = 0
             cookie_activity_map[key] += 1
 
-    top_keys = set()
+    top_keys = set() # Set of all top keys. A set is used to ensure no cookies are repeated.
     max_key = 0
-    for keys in cookie_activity_map:
+    for keys in cookie_activity_map: # Finds the top keys. Adds it to the map.
         count = cookie_activity_map[keys]
         if count > max_key:
             max_key = count
@@ -39,7 +43,7 @@ def most_active_cookie(selected_date):
         if count == max:
             top_keys.add(keys)
 
-    for result in top_keys:
+    for result in top_keys: # Prints all the top cookies line by line.
         print(result)
 
 
